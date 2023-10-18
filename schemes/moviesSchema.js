7c3e00180c1ff8dcaa68a112c99eaa2999b2d62f
Santiago Suarez <santiagosuarez.beater@outlook.com>
2023-10-17 20:33:05 -0500

    feat: :tada: First commit

---
+++ schemes/moviesSchema.js
@@ -0,0 +1,27 @@
+const { z } = require('zod')
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Title must be a string',
+    required_error: 'Title is required'
+  }),
+  year: z.number().int().min(1900).max(2077),
+  director: z.string(),
+  duration: z.number().int().positive(1),
+  poster: z.string().url(),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller'])
+  ),
+  rate: z.number().min(0).max(10).default(0)
+})
+
+function validateMovie (input) {
+  return movieSchema.safeParse(input)
+}
+function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
